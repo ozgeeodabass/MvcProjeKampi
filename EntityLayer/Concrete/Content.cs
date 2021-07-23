fbb3faace4661901bb263e9597b399ea5b3cdbe6
Özge Odabaş <180315051@ogr.cbu.edu.tr>
2021-07-23 18:14:22 +0300

    Add project files.

---
+++ EntityLayer/Concrete/Content.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Content
+    {
+        [Key]
+        public int ContentId { get; set; }
+        [StringLength(1000)]
+        public String ContentValue { get; set; }
+        public DateTime ContentDate { get; set; }
+        
+
+        //relational properties
+        public int HeadingId { get; set; }
+        public virtual Heading Heading { get; set; }
+
+        //nullable type
+        public int? WriterId { get; set; }
+        public virtual Writer Writer { get; set; }
+
+
+    }
+}
---
+++ EntityLayer/Concrete/Writer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Writer
+    {
+        [Key]
+        public int WriterId { get; set; }
+        [StringLength(50)]
+        public String WriterFirstName { get; set; }
+        [StringLength(50)]
+        public String WriterLastName { get; set; }
+        [StringLength(100)]
+        public String WriterImage { get; set; }
+        [StringLength(50)]
+        public String WriterMail { get; set; }
+        [StringLength(20)]
+        public String WriterPassword { get; set; }
+
+        //relational properties
+        public ICollection<Heading> Headings { get; set; }
+        public ICollection<Content> Contents { get; set; }
+    }
+}
