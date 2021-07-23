3227762dc03369ecbd05a20139bb5202291cd2f4
Özge Odabaş <180315051@ogr.cbu.edu.tr>
2021-07-23 19:10:31 +0300

    26/100

--- MvcProjeKampi/Controllers/CategoryController.cs
+++ MvcProjeKampi/Controllers/CategoryController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using EntityLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +11,30 @@ namespace MvcProjeKampi.Controllers
     public class CategoryController : Controller
     {
         // GET: Category
+        CategoryManager cm = new CategoryManager();
         public ActionResult Index()
         {
             return View();
+        }
+
+        
+        public ActionResult GetCategoryList()
+        {
+            var categoryValues = cm.GetAll();
+            return View(categoryValues);
+
+        }
+        [HttpGet]
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddCategory(Category c)
+        {
+            cm.Add(c);
+            return RedirectToAction("GetCategoryList");
         }
     }
 }
